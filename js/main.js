700979c5b3d0417f3a107b4b101cdf5c5e17a32e
hyejida <owlgpwl94@gmail.com>
2022-08-03 02:19:30 -0700

    first-scroll-height-JS

--- js/main.js
+++ js/main.js
@@ -49,13 +49,24 @@
     ];
 
     function setLayout() {
-        // Set the each scroll hieght
+        // *** Set the each scroll hieght
         for(let i=0; i < sceneInfo.length; i++) {
             // 다섯배를 하는 이유는 하나의 섹션이 진행되는동안 다른 섹션 애니메이션이 작동하지 않도록 하기 위해서 이다. 그래서 하나의 섹션을 길게 만듦.
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             // html 섹션 높이로 만들어주는것.(표현법)
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
+
+        // refresh website 초기화시 값
+        let totalScrollHeight = 0;
+        for(let i=0; i<sceneInfo.length; i++) {
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if(totalScrollHeight >= yOffset) {
+                currentScene=i;
+                break;
+            }
+        }
+        document.body.setAttribute('id', `show-secene-${currentScene}`)
     }
 
 
@@ -69,21 +80,26 @@
                                             // scene에 currentScene에 해당하는애
         if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
             currentScene++;
+            document.body.setAttribute('id', `show-secene-${currentScene}`)
         }
         if (yOffset < prevScrollHeight) {
-            if (currentScene===0) return;
+            if (currentScene===0) return; // For the mobile screen
             currentScene--;
+            document.body.setAttribute('id', `show-secene-${currentScene}`) 
         }
 
-        console.log(currentScene);
+        
      
     }
 
-    window.addEventListener('resize', setLayout);
+   
     window.addEventListener('scroll', () => {
         yOffset = window.pageYOffset;
         scrollLoop();
     });
 
-    setLayout();
-})();
+    window.addEventListener('DOMContentLoaded', setLayout);
+    window.addEventListener('resize', setLayout);
+    
+    
+})(); 
